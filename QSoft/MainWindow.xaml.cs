52893a7da061bd1d5a90bce7a1dcbcef6e22357c
Jitlee.Wan <www.wpj@163.com>
2013-02-27 00:40:14 +0800

    Signed-off-by: Jitlee.Wan <www.wpj@163.com>

--- QSoft/MainWindow.xaml.cs
+++ QSoft/MainWindow.xaml.cs
@@ -11,13 +11,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace QSoft
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
